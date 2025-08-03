dc51b698b25186507fc35a972c5f0bc126b99503
Azunanekita <maria.zapata.negocio@gmail.com>
2025-08-03 17:31:59 -0500

    guardar ultimas modificaciones

---
+++ Final-proyect/Menunavega/menujava.js
@@ -0,0 +1,9 @@
+function mostrarMenu() {
+  document.getElementById('slider').classList.add('oculto');
+  document.getElementById('seccionMenu').classList.remove('oculto');
+}
+
+function mostrarInicio() {
+  document.getElementById('slider').classList.remove('oculto');
+  document.getElementById('seccionMenu').classList.add('oculto');
+}
